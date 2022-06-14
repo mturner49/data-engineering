284d8388e9c1b3c13050328c9b7daa3bbc9ea95b
mturner49 <matt.turner@voxmedia.com>
2022-06-13 21:41:47 -0400

    updated 3rd assignment

--- assignments/module_two/pandas/excess_columns.py
+++ assignments/module_two/pandas/excess_columns.py
@@ -4,12 +4,12 @@ Your team just acquired another data source that, again, drops you daily files
 in your desired Google Cloud Storage landing bucket.
 
 Upon the initial data drop, you notice that the data the source provides contains PII (Personally Identifiable Information)
-that your team, legally, can not consume. The file also contain data formatting issues that will lead to problems when you
-ingest the files.
+that your team, legally, can not consume. The file also contains data formatting issues that will lead to problems when you
+ingest the files in your data warehouse
 
 Instructions:
 Using the csv file "data/pii_data.csv"
-1. Write a method to exclude the PII data (card_number, cvv)
+1. Write a method to exclude the PII columns (card_number, cvv)
 2. Change the date format from mm/dd/yyyy to yyyy-mm-dd
 '''
 
