31304e1bf3da168f42c1c6cc8b9a95d5b6ade411
mturner49 <matt.turner@voxmedia.com>
2022-06-22 17:32:22 -0400

    adding module three assignment

---
+++ assignments/module_three/README.md
@@ -0,0 +1,10 @@
+# Welcome to the Data Staging section
+
+In this section, you will be programmatically uploading your data files to an object store using Python. 
+Typically, organizations use S3 (AWS) or Google Cloud Storage (GCP)
+as their objects stores, but in this section, we will be using Google Drive.
+
+
+# Prerequisites
+- Install [PyDrive](https://pypi.org/project/PyDrive/)
+- Email thatdatacoach@gmail.com and request for credentials. You must have a gmail.
---
+++ assignments/module_three/upload_to_filestore.py
@@ -0,0 +1,13 @@
+'''
+Scenario:
+Now that you've been able to successfully extract the data files from source, you need to land
+those files in your company's object store. Your team is currently working on getting the AWS
+account activated and setup, but in the meantime, your team is utilizing Google Drive until
+AWS S3 becomes available.
+
+Instructions:
+Write a method that uploads your data files from the previous module to this
+Google Drive folder: https://drive.google.com/drive/folders/1Y40b8YmO41jIEmOw_la56wUDr56JKrmQ?usp=sharing 
+'''
+
+# Write your code below
